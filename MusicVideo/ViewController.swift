32843ddd7261a31421eef8811e47d1dafbff528c
christopher alphonse cervantes <iStand4JC@christophers-MacBook-Pro.local>
2017-05-29 10:58:44 +0800

    Initial Commit

---
+++ MusicVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicVideo
+//
+//  Created by christopher alphonse cervantes on 5/29/17.
+//  Copyright © 2017 christopher alphonse cervantes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
